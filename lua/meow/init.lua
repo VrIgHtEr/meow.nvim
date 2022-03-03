8365527315217b7fab4969e21f81159d8ead1fd4
Cedric Mamo <cedric.mamo.1@gov.mt>
2022-03-03 16:42:03 +0100

    refactor

--- lua/meow/init.lua
+++ lua/meow/init.lua
@@ -1,106 +1,119 @@
-local terminal = {}
+local kitty = {}
+local string, terminal, util = require 'toolshed.util.string', require 'meow.terminal', require 'toolshed.util'
 
-local uv = vim.loop
-local stdout = uv.new_tty(1, false)
-terminal.stdout = stdout
+kitty.constants = {
+    control_keys = {
+        format = 'f',
+        image_width = 's',
+        image_height = 'v',
+        compression = 'o',
+        transmission_medium = 't',
+        continuation = 'm',
+        read_offset = 'O',
+        read_length = 'S',
+        id = 'i',
+        placement_id = 'p',
+        action = 'a',
+        x_offset = 'X',
+        y_offset = 'Y',
+        clip_x = 'x',
+        clip_y = 'y',
+        clip_w = 'w',
+        clip_h = 'h',
+        display_rows = 'r',
+        display_cols = 'c',
+        z_index = 'z',
+        quiet = 'q',
+        cursor_mode = 'C',
+        delete = 'd',
+    },
+    format = { rgb = 24, rgba = 32, png = 100 },
+    compression = { zlib_deflate = 'z' },
+    transmission_medium = { direct = 'd', file = 'f', temp_file = 't', shared_memory = 's' },
+    continuation = { in_progress = '1', finished = '0' },
+    action = { query = 'q', delete = 'd', transmit = 't', transmit_and_display = 'T', put = 'p' },
+    quiet = { ok = '1', errors = '2' },
+}
 
-local queue = require('toolshed.util.generic.queue').new()
-local ansi = require 'meow.ansi'
-local writing = false
-local faulted, fault = false, nil
-local transaction_count = 0
-local cursor_saved = false
+function kitty.supported()
+    local term = vim.fn.getenv 'TERM'
+    return term == 'xterm-kitty' or term == 'wezterm'
+end
 
-local function commit()
-    local tbl, index = {}, 0
-    while queue:size() > 0 do
-        index = index + 1
-        tbl[index] = queue:dequeue()
+local valid_keys = {}
+for _, v in pairs(kitty.constants.control_keys) do
+    valid_keys[v] = true
+end
+
+local function chunks(data, size)
+    size = math.floor(math.abs(size or 4096))
+    assert(size > 0, 'size cannot be negative')
+    local len = data:len()
+    local blocks = math.floor((len + size - 1) / size)
+    local ret = {}
+    for i = 1, blocks do
+        local start = size * (i - 1)
+        ret[i] = data:sub(start + 1, math.min(len, start + size))
     end
-    vim.defer_fn(function()
-        local ret = uv.write(stdout, tbl)
-        if not ret then
-            writing = false
-            faulted = true
-            fault = ret
-            error(ret)
+    return ret
+end
+
+local function build_cmd(cmd)
+    local ret = {}
+    for k, v in pairs(cmd) do
+        if valid_keys[k] then
+            table.insert(ret, k .. '=' .. tostring(v))
         end
-    end, 0)
+    end
+    return table.concat(ret, ',')
 end
 
-local function write(data)
-    if not faulted then
-        if transaction_count > 0 or writing then
-            queue:enqueue(data)
-        else
-            queue:enqueue(data)
-            commit()
+function kitty.validate(opts)
+    opts = opts == nil and {} or opts
+    if type(opts) ~= 'table' then
+        return util.error('TYPE', type(opts))
+    end
+    for k, v in pairs(opts) do
+        if not valid_keys[k] then
+            return util.error('KEY', k)
+        end
+        local t = type(v)
+        if t ~= 'number' then
+            return util.error(k, 'TYPE', t)
         end
     end
+    return opts
 end
 
-function terminal.last_error()
-    return faulted, fault
-end
-function terminal.begin_transaction(dont_save_cursor)
-    if transaction_count == 0 and not dont_save_cursor then
-        cursor_saved = true
-        write(ansi.save_cursor())
-    end
-    transaction_count = transaction_count + 1
-end
-
-function terminal.end_transaction()
-    if transaction_count > 0 then
-        transaction_count = transaction_count - 1
-        if transaction_count == 0 then
-            if queue:size() > 0 then
-                commit()
-            end
-            if cursor_saved then
-                cursor_saved = false
-                write(ansi.restore_cursor())
-            end
+function kitty.send_cmd(cmd, data)
+    cmd = build_cmd(cmd)
+    terminal.begin_transaction(true)
+    cmd, data = cmd or '', chunks(string.base64_encode(data or ''))
+    local num_chunks = #data
+    if cmd == '' or num_chunks <= 1 then
+        local esc = { '\x1b_G', cmd, ';' }
+        if num_chunks == 1 then
+            table.insert(esc, data[1])
         end
-    end
-end
-
-function terminal.write(...)
-    for _, data in ipairs { ... } do
-        local t = type(data)
-        if t == 'string' then
-            write(data)
-        elseif t == 'table' then
-            local amt = #data
-            if amt > 0 then
-                for _, x in ipairs(data) do
-                    if type(x) ~= 'string' then
-                        error 'Invalid data in terminal write'
-                    end
+        table.insert(esc, '\x1b\\')
+        terminal.write(table.concat(esc))
+    else
+        for i = 1, num_chunks do
+            local esc = { '\x1b_G' }
+            if i == 1 then
+                table.insert(esc, cmd)
+                if cmd ~= '' then
+                    table.insert(esc, ',')
                 end
-                write(table.concat(data))
             end
-        elseif t == 'number' then
-            write(tostring(data))
-        else
-            error 'Invalid data in terminal write'
+            table.insert(esc, 'm=')
+            table.insert(esc, i == num_chunks and '0;' or '1;')
+            table.insert(esc, data[i])
+            table.insert(esc, '\x1b\\')
+            terminal.write(table.concat(esc))
         end
     end
-end
-
-function terminal.execute_at(row, col, func, ...)
-    terminal.begin_transaction()
-    write(ansi.cursor_position(row, col))
-    local ret = { pcall(func, row, col, ...) }
     terminal.end_transaction()
-    if not ret[1] then
-        return nil, ret[2]
-    end
-    return unpack(ret, 1)
 end
 
-function terminal.size()
-    return uv.tty_get_winsize(stdout)
-end
-
-return terminal
+return kitty

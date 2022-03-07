709186f145b8652726ad401a2daace692f9d0bd0
Cedric Mamo <cedric.mamo.1@gov.mt>
2022-03-07 15:37:55 +0100

    ioctl refactor

--- lua/meow/ioctl.lua
+++ lua/meow/ioctl.lua
@@ -13,8 +13,7 @@ ffi.cdef [[
 
 local wsize = ffi.cast('struct winsize *', ffi.C.malloc(8))
 function M.resolution()
-    local ret = ffi.C.ioctl(0, 21523, wsize) -- TIOCGWINSZ
-    return ret ~= 0 and { w = 0, h = 0, rows = 0, cols = 0 }
+    return ffi.C.ioctl(0, 21523, wsize) ~= 0 and { w = 0, h = 0, rows = 0, cols = 0 }
         or { h = wsize[0].ws_ypixel, w = wsize[0].ws_xpixel, rows = wsize[0].ws_row, cols = wsize[0].ws_col }
 end
 return M
